99e0da408e9ddd4deba0aced5ddf5d99e4034ab7
swarooppatilx <swarooppatil850@gmail.com>
2025-05-22 15:11:39 +0530

    feat: url based response

--- server.c
+++ server.c
@@ -8,6 +8,18 @@
 
 #define PORT 8080
 
+void send_response(int socket, const char* body){
+  char response[4096];
+  sprintf(response,
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Content-Length: %ld\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "%s",strlen(body), body);
+  write(socket, response, strlen(response));
+}
+
 int main(){
   int server_fd;
   int new_socket;
@@ -39,28 +51,38 @@ int main(){
   
   printf("Server Listining on port %d....\n",PORT);
 
-  //accept connections
-  new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-  if(new_socket<0){
-    perror("accept failed");
-    exit(EXIT_FAILURE);
+  while(1){
+   //accept connections
+   new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    if(new_socket<0){
+     perror("accept failed");
+     continue;
+    }
+
+   //read client request
+   char buffer[3000] = {0};
+   read(new_socket,buffer,3000);
+   printf("Request: \n%s\n", buffer);
+
+   //extract the requested path
+   char method[8], path[1024];
+   sscanf(buffer,"%s %s",method,path);
+   printf("Method: %s, Path: %s\n",method,path);
+   
+   //respond to path
+   if(strcmp(path,"/")==0){
+      send_response(new_socket, "<h1>home page</h1>");
+    }
+   else if(strcmp(path,"/about")==0){
+      send_response(new_socket, "<h1>about page</h1>");
+    }
+    else{
+      send_response(new_socket, "<h1>404</h1>");
+    }
+
+   //close socket
+   close(new_socket);
   }
-
-  //read client request
-  char buffer[3000] = {0};
-  read(new_socket,buffer,3000);
-  printf("Request: \n%s\n", buffer);
-
-  char* http_response = "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/html\r\n"
-                        "Connection: close\r\n"
-                        "\r\n"
-                        "<!DOCTYPE html><html><body><h1>Hello, World</h1></body></html>";
-  write(new_socket, http_response, strlen(http_response));
-  printf("Response Sent!\n");
-
-  //close socket
-  close(new_socket);
   close(server_fd);
   return 0;
 }
